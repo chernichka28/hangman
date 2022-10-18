255919ee9024a0935fccf5c2aa22ddf51480b9fd
chernichka28 <116021800+chernichka28@users.noreply.github.com>
2022-10-18 15:11:26 +0300

    Update game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -33,15 +33,11 @@ class Game
   # массиве на соответствующем месте находится nil. Этот массив нужен методу
   # экземпляра класса ConsoleInterface для вывода слова на игровом табло.
   def letters_to_guess
-    result =
-      @letters.map do |letter|
-        if @user_guesses.include?(normalize_letter(letter))
-          letter
-        else
-          nil
-        end
+    @letters.map do |letter|
+      if @user_guesses.include?(normalize_letter(letter))
+        letter
       end
-    result
+    end
   end
 
   # Возвращает true, если у пользователя не осталось ошибок, т.е. игра проиграна
